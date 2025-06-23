8f4c16d99d18f22d45751579348183970bc620a3
Boris Marinov <boris.marinov99@gmail.com>
2025-06-23 22:49:44 +0300

    Add Liveblocks auth and create documents and rooms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,47 @@
-import { Button } from "@/components/ui/button"
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+    const clearkUser = await currentUser();
+    if (!clearkUser) redirect('/sign-in');
+
+    const documents = [];
+
     return (
-        <div>
-            <Button>
-                Click ME
-            </Button>
-        </div>
+        <main className="home-container">
+            <Header className="sticky left-0 top-0">
+                <div className="flex items-center gap=2 lg:gap-4">
+                    Notification
+
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </Header>
+
+            {documents.length > 0 ? (
+                <div>da</div>
+            ) : (
+                <div className="document-list-empty">
+                    <Image
+                        src={'/icons/doc.svg'}
+                        alt={'Document'}
+                        width={40}
+                        height={40}
+                        className="mx-auto"
+                    />
+
+                    <AddDocumentBtn
+                        userId={clearkUser.id}
+                        email={clearkUser.emailAddresses[0]?.emailAddress || ''}
+                    />
+                </div>
+            )}
+        </main>
     )
 }
 
---
+++ components/Loader.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <Image
+                src="icons/loader.svg"
+                alt="loader"
+                width={32}
+                height={32}
+                className="animate-spin"
+                priority
+            />
+
+            Loading...
+        </div>
+    )
+}
+
+export default Loader;
