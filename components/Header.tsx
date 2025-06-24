f85b1374cb9f1d1575176195bf925e2eaed3818f
Boris Marinov <boris.marinov99@gmail.com>
2025-06-24 22:10:59 +0300

    Add possibility to edit document (room) title

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,18 +5,18 @@ import Link from "next/link"
 const Header = ({ children, className }: HeaderProps) => {
     return (
         <div className={cn("header", className)}>
-            <Link href="/" className="md:flex-1">
+            <Link href={"/"} className="md:flex-1">
                 <Image
-                    src="/icons/logo.svg"
-                    alt="Logo with name"
+                    src={"/icons/logo.svg"}
+                    alt={"Logo with name"}
                     width={120}
                     height={32}
                     className="hidden md:block"
                 />
 
                 <Image
-                    src="/icons/logo-icon.svg"
-                    alt="Logo"
+                    src={"/icons/logo-icon.svg"}
+                    alt={"Logo"}
                     width={32}
                     height={32}
                     className="mr-2 md:hidden"
